943497dcca81868cb93078001697903a50cda1d1
Felix Waller <54914900+f11xter@users.noreply.github.com>
2022-04-14 13:03:12 +0000

    Add persistent views Views have no timeout Do not persist over restarts

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import os
 import discord
-from discord.ui import Button, View
 from dotenv import load_dotenv
 from stay_awake import stay_awake
 from replit import db
-from functools import partial
+from datetime import datetime
 
 load_dotenv()
 BOT_TOKEN = os.getenv("TOKEN")
@@ -13,6 +12,66 @@ GUILD_ID = int(os.getenv("GUILD"))
 intents = discord.Intents.default()
 intents.members = True
 bot = discord.Bot(intents=intents)
+
+
+class PersistentView(discord.ui.View):
+    def __init__(self, user):
+        super().__init__(timeout=None)
+        self.user = user
+
+    @discord.ui.button(
+        label="Accept",
+        style=discord.ButtonStyle.green,
+        custom_id=f"mcsoc-bot-dev:button:accept:{datetime.now().isoformat()}",
+    )
+    async def accept(self, button: discord.ui.Button,
+                     interaction: discord.Interaction):
+        await accept_click(interaction)
+
+    @discord.ui.button(
+        label="Reject",
+        style=discord.ButtonStyle.red,
+        custom_id=f"mcsoc-bot-dev:button:reject:{datetime.now().isoformat()}",
+    )
+    async def reject(self, button: discord.ui.Button,
+                     interaction: discord.Interaction):
+        await reject_click(interaction, self.user)
+
+
+class SelectRejectionReasonView(discord.ui.View):
+    def __init__(self, user):
+        self.user = user
+        super().__init__()
+
+    @discord.ui.select(
+        placeholder="Select a reason for rejection",
+        min_values=1,
+        max_values=1,
+        options=[
+            discord.SelectOption(label="Not Registered On SU"),
+            discord.SelectOption(
+                label="Used IT username instead of numerical student ID"),
+            discord.SelectOption(label="Non-existent Minecraft username"),
+            discord.SelectOption(label="Non Student Email"),
+            discord.SelectOption(label="Incorrect Details"),
+            discord.SelectOption(label="Other Rejection Reason")
+        ])
+    async def select_callback(self, select, interaction):
+        await interaction.message.edit(content=interaction.message.content +
+                                       "```diff\n- Rejected - " +
+                                       select.values[0] + "\n```",
+                                       view=None)
+
+        try:
+            user_dm = await bot.create_dm(self.user)
+            await user_dm.send(
+                "You were not whitelisted on the UoM Minecraft Society server "
+                "due to an issue with the details you entered. The issue was: "
+                f"{select.values[0]}. Please correct this and send the "
+                "whitelist command again, or contact a committee member if "
+                "you need help.")
+        except Exception:
+            print("Failed to send DM")
 
 
 @bot.event
@@ -29,19 +88,8 @@ async def whitelist_user(ctx, username, email_or_id, email: bool):
     rules_channel = bot.get_channel(int(os.getenv("RULES-CHANNEL")))
     info_channel = bot.get_channel(int(os.getenv("INFO-CHANNEL")))
 
-    view = View()
-    accept_button = Button(label="Accept", style=discord.ButtonStyle.green)
-    accept_button.callback = accept_click
-    view.add_item(accept_button)
-
-    reject_reasons = ["Not Registered On SU", "Used IT username instead of numerical student ID",
-                      "Non-existent Minecraft username", "Non Student Email", "Incorrect Details",
-                      "Other Rejection Reason"]
-    for reject_reason in reject_reasons:
-        reject_button = Button(label=reject_reason, style=discord.ButtonStyle.red)
-        reject_button_callback_partial = partial(reject_click, reject_reason=reject_reason, user=ctx.author)
-        reject_button.callback = reject_button_callback_partial
-        view.add_item(reject_button)
+    view = PersistentView(user=ctx.author)
+    bot.add_view(view)
 
     await log_channel.send(
         f"Discord user: {ctx.author.mention}\n"
@@ -59,9 +107,7 @@ async def whitelist_user(ctx, username, email_or_id, email: bool):
 
 
 # Create whitelist command group
-whitelist = bot.create_group(
-    "whitelist",
-    guild_ids=[GUILD_ID])
+whitelist = bot.create_group("whitelist", guild_ids=[GUILD_ID])
 
 
 @whitelist.command(guild_ids=[GUILD_ID])
@@ -78,41 +124,30 @@ async def with_email(ctx, minecraft_username, student_email):
 
 async def accept_click(interaction):
     msg = interaction.message.content.split()
-    username = msg[2]
-    mc_user = db[username][1]
+    user_id = msg[2]
+    mc_user = db[user_id][1]
 
     # Send whitelist command
     console_channel = bot.get_channel(int(os.getenv("CONSOLE-CHANNEL")))
     await console_channel.send(f"whitelist add {mc_user}")
 
     guild = interaction.guild
-    member = await guild.fetch_member(username[2:-1])
-    
+    member = await guild.fetch_member(user_id[2:-1])
+
     # Add whitelisted role
     whitelisted_role = guild.get_role(int(os.getenv("WHITELISTED-ROLE")))
     await member.add_roles(whitelisted_role)
 
     # Remove buttons and record result
-    await interaction.message.edit(
-        content=interaction.message.content + "```diff\n+ Accepted\n```",
-        view=None
-    )
+    await interaction.message.edit(content=interaction.message.content +
+                                   "```diff\n+ Accepted\n```",
+                                   view=None)
 
 
-async def reject_click(interaction, reject_reason, user):
-    # Remove buttons and record result
+# async def reject_click(interaction, reject_reason, user):
+async def reject_click(interaction, user):
     await interaction.message.edit(
-        content=interaction.message.content + "```diff\n- Rejected - " + reject_reason + "\n```",
-        view=None
-    )
-    try:
-        user_dm = await bot.create_dm(user)
-        await user_dm.send(
-            "You were not whitelisted on the UoM Minecraft Society server due to an issue with the details you entered."
-            f" The issue was: {reject_reason}. Please correct this and send the whitelist command again, "
-            "or contact a committee member if you need help.")
-    except Exception:
-        print("Failed to send DM")
+        view=SelectRejectionReasonView(user))
 
 
 stay_awake()
